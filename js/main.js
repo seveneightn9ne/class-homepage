b45bb124ec658b945dafc00c02bb477b13854085
Jess Kenney <seveneightn9ne@gmail.com>
2013-09-10 18:17:55 -0400

    init,v0.2

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	$("object#page").height($(window).height()).width($("article.main").width())
+
+	$("nav.main").height($(window).height())
+
+    $("nav.main a").click(function(){
+        $("article.main")
+        	.empty()
+        	.append($("<object>")
+        		.attr("type","text/html")
+        		.attr("id", "page")
+        		.attr("height", $(window).height())
+        		.attr("width", $("article.main").width())
+        		.attr("data",$(this).attr("href"))
+        	)
+        return false
+    })
+
+    // $("#tt").click(function(){
+    // 	$("article.main")
+    // 		.empty()
+    // 		.append($("<object>")
+    //     		.attr("type","text/html")
+    //     		.attr("id", "page")
+    //     		.attr("height", $(window).height())
+    //     		.attr("width", $("article.main").width()-700)
+    //     		.attr("data",$(this).attr("data-todoist"))
+    //     		.css("float","left")
+    // 		)
+    // 		.append($("<object>")
+    //     		.attr("type","text/html")
+    //     		.attr("id", "page")
+    //     		.attr("height", $(window).height())
+    //     		.attr("width", "700px")
+    //     		.attr("data",$(this).attr("data-toggle"))
+    //     		.css("float","left")
+    // 		)
+    // 	return false
+    // })
+
+    $("#tt").trigger("click")
+
+    $(window).resize(function(){
+    	if($("article.main > *").size() == 1){
+    		$("article.main > *")
+    			.attr("height", $(window).height())
+    			.attr("width", $("article.main").width())
+    	}
+    	else {
+    		$("article.main > * ").first()
+    			.attr("height", $(window).height())
+    			.attr("width", $("article.main").width()-700)
+    		$("article.main > *").last()
+    			.attr("height", $(window).height())
+    			.attr("width", "700px")
+    	}
+    })
+})
